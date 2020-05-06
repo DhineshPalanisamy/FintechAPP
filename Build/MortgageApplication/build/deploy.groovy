456cda190a46beeaaa9f000c432fe791a53b0cdc
Dhinesh Palanisamy <dhinesh.palanisamy@tcs.com>
2020-05-06 09:08:35 -0500

    Change by Dhinesh V60 build prop and deploy groovy

--- Build/MortgageApplication/build/deploy.groovy
+++ Build/MortgageApplication/build/deploy.groovy
@@ -113,7 +113,7 @@ def cmd = [ properties.buztoolPath,
 	"$properties.workDir/shiplist.xml",
         //requires UCD v6.2.6 and above
         "-ar",
-        properties.artifactrepository,
+        "$properties.artifactrepository",
 	"-o",
 	"${properties.workDir}/buztool.output"
     	]
